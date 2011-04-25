076f8cf27d3a1b52a1b597e224d23bd2ba18fcd7
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2011-04-25 13:00:21 +0200

    Use [a-z]* pattern to match project ids

--- kalamarsite.py
+++ kalamarsite.py
@@ -8,17 +8,18 @@ from sitenco import PROJECTS_PATH
 
 page = Rest(
         FileSystem(
-            PROJECTS_PATH, r'(.*)/pages/(.*)\.rst', ('project', 'page')),
+            PROJECTS_PATH, r'([a-z]*)/pages/(.*)\.rst', ('project', 'page')),
         [('title', RestProperty(unicode, TITLE))], 'content')
 news = Rest(
     FileSystem(
-        PROJECTS_PATH, r'(.*)/news/(.*)/(.*)\.rst',
+        PROJECTS_PATH, r'([a-z]*)/news/(.*)/(.*)\.rst',
         ('project', 'writer', 'datetime')),
     [('title', RestProperty(unicode, TITLE))], 'content')
 tutorial = Cache(
     Rest(
         FileSystem(
-            PROJECTS_PATH, r'(.*)/tutorials/(.*)\.rst', ('project', 'tutorial')),
+            PROJECTS_PATH,
+            r'([a-z]*)/tutorials/(.*)\.rst', ('project', 'tutorial')),
         [('title', RestProperty(unicode, TITLE)),
          ('abstract', RestProperty(unicode, '//topic/paragraph'))], 'content'))
                      
