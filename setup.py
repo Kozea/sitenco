50d352bb4bae94a11a584c3e527f50bf760ef5ed
Simon Sapin <simon.sapin@exyr.org>
2011-05-26 16:36:40 +0200

    Add a setup.py to install dependencies.

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="SiteNCo",
+    packages=find_packages(),
+    install_requires=["Dyko >= 0.3", "CSStyle", "Flask", "docutils"])
+        
