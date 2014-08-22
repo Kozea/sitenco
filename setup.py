9a285a871532242b2bc683cc8f985342d923a15c
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2014-08-22 15:21:30 +0200

    Remove useless scripts

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="SiteNCo",
-    packages=find_packages(),
-    install_requires=["Flask", "docutils-html5-writer", "pyyaml", "pygal"])
